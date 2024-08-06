3dcc7c646f3a8c9a81332559a96d658c384b7337
AKHIL-DyC <akhiljee666@gmail.com>
2024-08-06 20:25:35 +0530

    firstpage

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,10 +1,23 @@
-import Image from "next/image";
+"use client"
+import Image from 'next/image';
+import AnimatedText from './component/Animatedtext';
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className="flex flex-col">
+      <div style={{height:'60vh',width:'100vw',display:'flex',alignContent:'start'}}>
+        <AnimatedText/>
+      </div>
+      <div className="flex w-full h-23vh flex-col items-center justify-center">
+        <div className="flex justify-center text-center">
+          <h2 className="text-2xl font-semibold">We partnered with events like</h2>
+        </div>
+        <div className="flex w-1/2 justify-between items-center text-center mt-4">
+          <Image src={'/logo-final.png'}alt='nsdc'width={200}height={200}></Image>
+          <Image src={'/ieee.png'}alt='ieee'width={200}height={200}></Image>
+          <Image src={'/ihrd.jpg'}alt='ihrd'width={200}height={200}></Image>
+        </div>
+      </div>
     </div>
-
   );
 }
