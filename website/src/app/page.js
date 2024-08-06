08c70f4101dcde6fe6e6f62972046b55a24ab1b1
AKHIL-DyC <akhiljee666@gmail.com>
2024-08-06 21:01:28 +0530

    added faq

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import Image from 'next/image';
 import AnimatedText from './component/Animatedtext';
-
+import Collapsible from './component/Collapsable';
 export default function Home() {
   return (
     <div className="flex flex-col">
@@ -17,6 +17,23 @@ export default function Home() {
           <Image src={'/ieee.png'}alt='ieee'width={200}height={200}></Image>
           <Image src={'/ihrd.jpg'}alt='ihrd'width={200}height={200}></Image>
         </div>
+
+        <div style={{width:'70vw',display:'flex',flexDirection:'column',gap:'4vh'}}>
+      <Collapsible title="What is the refund policy?">
+        All purchases are final and non-refundable. However, we offer a 30-day money-back guarantee if you are not
+        satisfied with your purchase. Please contact our support team for more information.
+      </Collapsible>
+      <Collapsible title="How do I contact customer support?">
+        You can reach our customer support team by email at support@example.com or by phone at 1-800-555-1234. Our
+        support team is available Monday through Friday, 9am to 5pm EST.
+      </Collapsible>
+      <Collapsible title="What payment methods do you accept?">
+        We accept all major credit cards, including Visa, Mastercard, American Express, and Discover. We also accept
+        PayPal and Apple Pay.
+      </Collapsible>
+    </div>
+
+
       </div>
     </div>
   );
