75886bc6e23a51e38df861f89c49e77033e81e70
alanjohnck <alanjohnchacko.live@gmail.com>
2024-09-13 23:43:54 +0530

    font changed& registration section

--- website/src/app/layout.js
+++ website/src/app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
+import { Inter, Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import Navbar from "./component/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto_Condensed({subsets:["latin"]})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="overflow-x-hidden">
+    <html lang="en" className="overflow-x-hidden ">
       <body className={inter.className}>
         <Navbar/>
         {children}
